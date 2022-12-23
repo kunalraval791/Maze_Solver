1f12220041e586642a8f1cf87b9acf2a813c46d9
kunalraval791 <121260919+kunalraval791@users.noreply.github.com>
2022-12-23 21:23:04 +0530

    Initial commit

---
+++ src/main/java/mazesolver/NewClass.java
@@ -0,0 +1,67 @@
+package mazesolver;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author DELL
+ */
+public class NewClass extends JFrame {
+    int[][] maze = {{1,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,0,1,0,1,1,1,0,0,0,0,0,1},
+            {1,0,1,1,1,1,1,0,1,1,1,0,1},
+            {1,0,1,1,1,1,1,0,0,0,0,0,1},
+            {1,0,0,0,0,0,0,0,1,1,1,0,1},
+            {1,0,1,0,1,1,1,0,1,1,1,0,1},
+            {1,0,1,0,1,1,1,0,1,1,1,0,1},
+            {1,0,1,0,1,1,1,0,1,1,1,0,1},
+            {1,0,0,0,0,0,0,0,0,0,1,9,1},
+            {1,1,1,1,1,1,1,1,1,1,1,1,1}
+    };
+
+
+    public List<Integer> path = new ArrayList<>();
+    NewClass(){
+        setTitle("Maze Solver");
+        setSize(640, 480);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Dfs.searchPath(maze, 1, 1, path);
+        System.out.println(path);
+    }
+
+    @Override
+    public void paint(Graphics g){
+        g.translate(50, 50);
+        super.paint(g);
+        for(int i = 0; i < maze.length; i++){
+            for(int j = 0; j < maze[0].length; j++){
+                Color color;
+                switch(maze[i][j]){
+                    case 1: color = Color.BLACK; break;
+                    case 9: color = Color.RED; break;
+                    default: color = Color.WHITE; break;
+                }
+                g.setColor(color);
+                g.fillRect(30*j, 30*i, 30, 30);
+                g.setColor(Color.RED);
+                g.drawRect(30*j,30*i,30,30);
+            }
+        }
+        for (int i = 0; i < path.size(); i+=2) {
+            int pathx = path.get(i);
+            int pathy = path.get(i+1);
+            g.setColor(Color.GREEN);
+            g.fillRect(30*pathx, 30*pathy, 30, 30);
+        }
+    }
+
+    public static void main(String[] args){
+        NewClass view = new NewClass();
+        view.setVisible(true);
+    }
+
+}
